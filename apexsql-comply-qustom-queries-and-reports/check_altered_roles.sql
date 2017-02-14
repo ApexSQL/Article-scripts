b71c9180785b85198c606e80d83d27b912229636
ApexSQLAlphaTeam <alpha@apexsql.com>
2017-02-14 12:44:40 +0100

    summary
    Companion scripts

--- apexsql-comply-qustom-queries-and-reports/check_altered_roles.sql
+++
@@ -1,6 +0,0 @@
-SELECT LoginName, CreateTime, TextData 
-AS     EventCount 
-FROM   ApexSQLCrd.ApexSql.EventView
-WHERE  TextData
-LIKE  'ALTER SERVER ROLE%'
-AND    ServerName = 'FUJITSUSQL2012'
--- apexsql-comply-qustom-queries-and-reports/check_number_of_audited_events.sql
+++
@@ -1,7 +0,0 @@
-SELECT LoginName, count (ID) 
-AS EventCount 
-FROM ApexSQLCrd.ApexSql.EventView 
-WHERE CreateTime
-      > 
-      '2013-07-03' AND LoginName is not null
-GROUP BY LoginName
